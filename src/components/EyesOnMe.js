49412cf2639b122e9e53919f26cf3bc25975dfc8
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-06-01 15:55:43 -0500

    commit tree da056591668dea4e550fbff6d5217d616ff955a7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    handleBlur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    handleFocus = () => {
+        console.log('Good!')
+    }
+
+    render() {
+        return(
+            <button onBlur={this.handleBlur} onFocus={this.handleFocus}>Focus Button</button>
+        )
+    }
+
+}
+
+export default EyesOnMe
